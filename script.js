511c043e80c7bc27597259a00e6903cb3deff6ab
Adelange2020 <antondelange2020@gmail.com>
2025-01-03 21:19:53 +0100

    working

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-
 let humanScore = 0;
 let computerScore = 0;
 let rounds = 0;
 
-game();
 
 function game() {
     let human = "";
     let computer = "";
-
     do {
         human = getHumanChoice();
         computer = getComputerChoice();
-    } while (rounds < 5) 
-        
-    }
-
-    if (human == computer) {
-        console.log("its a draw, please play again");
-    } else if ((human == "rock" && computer == "paper")(human == "scissors" && computer == "rock")) {
-        rounds++;
-        computerScore++;
-    }else {
-        rounds++;
-        humanScore++;
-    }
-
-console.log("computer: " + computer + " VS " + human)
-
+        if (human == computer) {
+            alert("Its a draw, please play again");
+        }
+        else
+        {
+            if (human == "rock" && computer == "paper")
+            {
+                computerScore++;
+                rounds++;
+                console.log("Computer had "+computer+" and has "+computerScore+ " points now.");
+                
+            }
+            if (human == "sissors" && computer == "paper")
+            {
+                humanScore++;
+                rounds++;
+                console.log("Human had "+human+"  and has "+humanScore+ " points now.");
+            }
+            if (human == "paper" && computer == "scissors")
+            {
+                computerScore++;
+                rounds++;
+                console.log("Computer had "+computer+" and has "+computerScore+ " points now.");
+                
+            }
+            if (human == "sissors" && computer == "paper")
+            {
+                humanScore++;
+                rounds++;
+                console.log("Human had "+human+"  and has "+humanScore+ " points now.");
+            }
+        }
+    } while (rounds < 5)
+    console.log("Game over.\nComputer: "+computerScore+ "\nHuman: "+humanScore);
+}
 
 function getComputerChoice () {
     let random = Math.floor(Math.random() * 3);
@@ -44,5 +60,4 @@ function getComputerChoice () {
 function getHumanChoice () {
     let choice = prompt("Enter rock, paper or scissors").toLowerCase()
     return choice;
-
 }
